d263ad9e2b860b52c7d958f04474ba778eccff2f
yjcstudy1 <153605289+yjcstudy1@users.noreply.github.com>
2023-12-13 19:00:35 +0800

    1213

--- url.js
+++ url.js
@@ -10,7 +10,7 @@
 const urls = [
 	'https://docs.google.com/forms/d/1P2gg38sCikIKH6DsP6dB4FQfyNeF2lPRnZKp0t_GOpI/viewform?edit_requested=true（植物肉植物蛋白）',
 	'https://docs.google.com/forms/d/1B72J_64wUrb4b5Wk5p2iOuBDl33q3Pk3vnl3N6Y4TXw/viewform?edit_requested=true（植物肉豌豆蛋白）',
-	'https://docs.google.com/forms/d/1ksl3FykK6oVPckbxY50-n9-yjqRugUstg4jRfGmbD8s/viewform?edit_requested=true&fbzx=-8512746981620111143（未來肉植物蛋白）',
+	'https://docs.google.com/forms/d/1ksl3FykK6oVPckbxY50-n9-yjqRugUstg4jRfGmbD8s/viewform?edit_requested=true（未來肉植物蛋白）',
 	'https://docs.google.com/forms/d/18BlmJlHmeeKhluKPV5L5ckwTzMy0CnQwukX35IG9AIc/viewform?edit_requested=true（未來肉豌豆蛋白）',
 	'https://docs.google.com/forms/d/1ciCrDRSkUrNZoUPn_WhukgjA7HSovh0jsDY2KI8jXe8/viewform?edit_requested=true（新創肉植物蛋白）',
 	'https://docs.google.com/forms/d/1sHk8JKU8ekwxiWJar-t2fdn4fJNKQ4XfRwI_F4OGbC0/viewform?edit_requested=true（新創肉豌豆蛋白）'
