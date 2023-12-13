d01d7fcf3a12c04010709a449a49d60cff5d744c
yjcstudy1 <153605289+yjcstudy1@users.noreply.github.com>
2023-12-13 18:44:25 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,10 +8,10 @@
 
 
 const urls = [
-	'https://images.chinatimes.com/newsphoto/2023-10-31/656/20231031004996.jpg',
-	'https://images.chinatimes.com/newsphoto/2021-03-25/1024/20210325003285.jpg',
-	'https://images.chinatimes.com/newsphoto/2023-10-20/1024/20231020001810.jpg',
-	'https://images.chinatimes.com/newsphoto/2023-07-24/1024/20230724002060.jpg',
-	'https://images.chinatimes.com/newsphoto/2023-07-27/1024/BD0100_P_02_02.jpg',
-	'https://images.chinatimes.com/newsphoto/2023-10-24/1024/20231024005254.jpg'
+	'https://docs.google.com/forms/d/1P2gg38sCikIKH6DsP6dB4FQfyNeF2lPRnZKp0t_GOpI/viewform?edit_requested=true（植物肉植物蛋白）',
+	'https://docs.google.com/forms/d/1B72J_64wUrb4b5Wk5p2iOuBDl33q3Pk3vnl3N6Y4TXw/viewform?edit_requested=true（植物肉豌豆蛋白）',
+	'https://docs.google.com/forms/d/1ksl3FykK6oVPckbxY50-n9-yjqRugUstg4jRfGmbD8s/viewform?edit_requested=true&fbzx=-8512746981620111143（未來肉植物蛋白）',
+	'https://docs.google.com/forms/d/18BlmJlHmeeKhluKPV5L5ckwTzMy0CnQwukX35IG9AIc/viewform?edit_requested=true（未來肉豌豆蛋白）',
+	'https://docs.google.com/forms/d/1ciCrDRSkUrNZoUPn_WhukgjA7HSovh0jsDY2KI8jXe8/viewform?edit_requested=true（新創肉植物蛋白）',
+	'https://docs.google.com/forms/d/1sHk8JKU8ekwxiWJar-t2fdn4fJNKQ4XfRwI_F4OGbC0/viewform?edit_requested=true（新創肉豌豆蛋白）'
 ];
